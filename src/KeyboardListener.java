b42cabba3a6f6def3414b0d2cbc07d5bddfb4c6c
student <student@192.168.117.111>
2014-05-15 20:21:59 -0400

    enabled keys A, S, D, W to be used in addition to arrow keys

--- src/KeyboardListener.java
+++ src/KeyboardListener.java
@@ -4,23 +4,28 @@ import java.awt.event.KeyEvent;
  public class KeyboardListener extends KeyAdapter{
  	
  		public void keyPressed(KeyEvent e){
- 		    switch(e.getKeyCode()){
-		    	case KeyEvent.VK_RIGHT:	 
+ 		    switch(e.getKeyCode())
+ 		    {
+		    	case KeyEvent.VK_RIGHT:	
+		    	case KeyEvent.VK_D:	
 		    				//if it's not the opposite direction
 		    				if(ThreadsController.directionSnake!=2) 
 		    					ThreadsController.directionSnake=1;
 		    			  	break;
 		    	case KeyEvent.VK_UP:	
+		    	case KeyEvent.VK_W:	
 							if(ThreadsController.directionSnake!=4) 
 								ThreadsController.directionSnake=3;
 		    				break;
 		    				
-		    	case KeyEvent.VK_LEFT: 	
+		    	case KeyEvent.VK_LEFT:
+		    	case KeyEvent.VK_A:
 							if(ThreadsController.directionSnake!=1)
 								ThreadsController.directionSnake=2;
 		    				break;
 		    				
-		    	case KeyEvent.VK_DOWN:	
+		    	case KeyEvent.VK_DOWN:
+		    	case KeyEvent.VK_S:
 							if(ThreadsController.directionSnake!=3)
 								ThreadsController.directionSnake=4;
 		    				break;
